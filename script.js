c7fdf46f3564d1bae0633fed81650978cfb1854a
Richard <richard031104@outlook.com>
2022-06-12 16:59:10 -0400

    Started with Header HTML / CSS

---
+++ script.js
@@ -0,0 +1,24 @@
+
+class Book {
+    constructor(
+      title = 'Unknown',
+      author = 'Unknown',
+      pages = '0',
+      isRead = false
+    ) {
+      this.title = title
+      this.author = author
+      this.pages = pages
+      this.isRead = isRead
+    }
+  }
+
+  class Library {
+    constructor() {
+        this.books = []
+    }
+  }
+
+//   Code
+
+const library = new Library();
